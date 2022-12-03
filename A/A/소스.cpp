44b2e1330f0984a946b1260347e81973acde5586
이지호 <jiho9707@gmail.com>
2022-12-03 15:51:01 +0900

    A번

--- A/A/소스.cpp
+++ A/A/소스.cpp
@@ -1,179 +1,222 @@
 #include <iostream>
-#include <math.h>
-#include <vector>
 #include <stdio.h>
+
 using namespace std;
 
-#define MAX_D 101
-#define MAX(a, b) ((a > b) ? (a):(b))
+//param
+int t;
+
+//test case
+/*
+1
+2 9 3 7 2
+2 9 1 8 0
+
+*/
+
+typedef struct {
+	int n;
+	int co[101];
+	int in[101];
+}poly;
 
 int n;
 
-typedef struct {
-	int degree;
-	int coef[MAX_D];
-	int epoonent[MAX_D];
-}poly;
-
-//poly poly_minus(poly A, poly B) {
-//	poly C;
-//	int Apos = 0, Bpos = 0, Cpos = 0;
-//	int degree_a = A.degree;
-//	int degree_b = B.degree;
-//	C.degree = MAX(A.degree, B.degree);
-//
-//	while (Apos <= A.degree && Bpos <= B.degree) {
-//		if (degree_a > degree_b) {
-//			C.coef[Cpos++] = A.coef[Apos++];
-//			degree_a--;
-//		}
-//		else if (degree_a == degree_b) {
-//			C.coef[Cpos++] = A.coef[Apos++] - B.coef[Bpos++];
-//			degree_a--;
-//			degree_b--;
-//		}
-//		else {
-//			C.coef[Cpos++] = B.coef[Bpos++];
-//			degree_b--;
-//		}
-//	}
-//	return C;
-//}
-
-poly poly_minus(poly A, poly B) {
-	poly C;
-
-	C.degree = MAX(A.degree, B.degree);
-	int i = 0;
-	int j = 0;
-	int cnt = C.degree;
-
-	
-	for (int k = 0; k < cnt; k++) {
-
-		if (A.epoonent[i] == B.epoonent[j]) {
-			//cout << 1 << ":";
-			C.coef[k] = A.coef[i] - B.coef[j];
-			//cout << C.coef[k] << endl;
-			C.epoonent[k] = A.epoonent[i];
-			i++;
-			j++;
-		}
-		else if (A.epoonent[i] > B.epoonent[j]) {
-			//cout << 2 << " : " << endl;
-			C.coef[k] = A.coef[i];
-			C.epoonent[k] = A.epoonent[i];
-			i++;
-		}
-		else if (A.epoonent[i] < B.epoonent[j]) {
-			//cout << 3 << " : " << endl;
-			//cout << A.epoonent[i] << endl;
-			C.coef[k] = 0 - B.coef[j];
-			//cout << C.coef[k] << endl;
-			C.epoonent[k] = B.epoonent[j];
-			j++;
-		}
+int comp(int a, int b) {
+	if (a > b) {
+		return a;
 	}
-	
-
-	/*for (int i = 0; i < cnt; i++) {
-		cout << "C의 coef값" << C.coef[i] << " ";
+	else {
+		return b;
 	}
-	cout << endl;
-	for (int i = 0; i < cnt; i++) {
-		cout << "C의 epo값" << C.epoonent[i] << " ";
-	}
-	cout << endl;*/
-	return C;
 }
 
-void print_poly(poly p) {
-	bool b = false;
-	for (int i = 0; i < p.degree-1; i++) {
+poly poly_sub(poly a, poly b) {
 
-		if (p.coef[i] == 1 || p.coef[i] == -1) {
-			if (p.epoonent[i] == 1) {
-				printf("x");
-			}
-			else {
-				printf("x^%d", p.epoonent[i]);
-			}
-		}
-		else if (p.epoonent[i] == 1) {
-			printf("%dx", abs(p.coef[i]));
-		}
-		else {
-			printf("%dx^%d", abs(p.coef[i]), p.epoonent[i]);
-		}
-		if (p.coef[i + 1] >= 0) {
-			printf("+");
-		}
-		else {
-			printf("-");
-		}
-	}
-	if(p.epoonent[p.degree-1] == 0) {
-		printf("%d", abs(p.coef[p.degree - 1]));
-	}
-	if (p.coef[p.degree - 1] == 0) {
-		printf("%d", 0);
-	}
-	cout << endl;
-}
+	poly p = {};
+	//cout << "p.n = " << p.n << endl;
 
-poly make_poly(vector<int> &v) {
-	poly p;
-	p.degree = v[0];
-	for (int i = 1; i < v.size(); i += 2) {
-		p.coef[i/2] = v[i];
-		p.epoonent[i/2] = v[i+1];
+	
+	int i = 0, j = 0, k = 0;
 
-		//cout << i << " = " << p.coef[i/2] << ":" << v[i] << " " << p.epoonent[i/2] << ":" << v[i + 1] << endl;
+	while(true) {
+		if (a.in[i] == b.in[j]) {
+			p.in[k] = a.in[i];
+			p.co[k] = a.co[i] - b.co[j];
+			i++;
+			j++;
+			k++;
+		}
+		else if (a.in[i] > b.in[j]) {
+			p.in[k] = a.in[i];
+			p.co[k] = a.co[i];
+			i++;
+			k++;
+		}
+		else if (a.in[i] < b.in[j]) {
+			p.in[k] = b.in[j];
+			p.co[k] = -b.co[j];
+			j++;
+			k++;
+		}
+
+		if (i == a.n && j == b.n) {
+			p.n = k;
+			break;
+		}
 	}
+
 	return p;
 }
 
-int main() {
-	cin >> n;
+void print_poly(poly c) {
 
-	for (int i = 0; i < n; i++) {
-		vector<int> v1;
-		vector<int> v2;
-		poly p1;
-		poly p2;
+	for (int i = 0; i < c.n; i++) {
+
+		if (c.co[i] == 0 && i == c.n - 1) {
+			printf("0");
+			continue;
+		}
+		if (c.co[i] == 0 && i != c.n - 1) {
+			continue;
+		}
+
+		if (c.co[i] > 0) {
+			if (c.co[i] == 1) {
+				if (c.in[i] == 0) {
+					printf("%d", c.co[i]);
+				}
+				else if (c.in[i] == 1) {
+					printf("x");
+				}
+				else {
+					printf("x^%d", c.in[i]);
+				}
+			}
+			else {
+				if (c.in[i] == 0) {
+					printf("%d", c.co[i]);
+				}
+				else if (c.in[i] == 1) {
+					printf("%dx", c.co[i]);
+				}
+				else {
+					printf("%dx^%d", c.co[i], c.in[i]);
+				}
+			}
+		}
+
+		if (c.co[i] < 0) {
+			if (c.co[i] == -1) {
+				if (c.in[i] == 0) {
+					printf("%d", c.co[i]);
+				}
+				else if (c.in[i] == 1) {
+					printf("-x");
+				}
+				else {
+					printf("-x^%d", c.in[i]);
+				}
+			}
+			else {
+				if (c.in[i] == 0) {
+					printf("%d", c.co[i]);
+				}
+				else if (c.in[i] == 1) {
+					printf("%dx", c.co[i]);
+				}
+				else {
+					printf("%dx^%d", c.co[i], c.in[i]);
+				}
+			}
+		}
+		if (i < c.n - 1) {
+			if (c.co[i + 1] >= 0) {
+				printf("+");
+			}
+		}
+	}
+	/*cout << endl;
+	cout << endl;
+	cout << endl;*/
+}
+
+int main() {
+	cin >> t;
+
+	while (t--) {
+
+		poly c = {};
+		poly p1 = {};
+		poly p2 = {};
+
 		for (int i = 0; i < 2; i++) {
+			int nterm;
+			cin >> nterm;
 			if (i == 0) {
-				int nterm;
-				cin >> nterm;
-				v1.push_back(nterm);
-				while (nterm--) {
+				p1.n = nterm;
+				for (int i = 0; i < nterm*2; i++) {
+					p1.co[i] = 0;
+					p1.in[i] = -1;
+				}
+				for (int j = 0; j < nterm; j++) {
 					int a, b;
 					cin >> a >> b;
-					v1.push_back(a);
-					v1.push_back(b);
+					p1.co[j] = a;
+					p1.in[j] = b;
 				}
-
-				p1 = make_poly(v1);
-				/*print_poly(p1);
+				/*cout << p1.n << endl;
+				for (int j = 0; j < nterm; j++) {
+					cout << p1.co[j] << " ";
+				}
+				cout << endl;
+				for (int j = 0; j < nterm; j++) {
+					cout << p1.in[j] << " ";
+				}
 				cout << endl;*/
 			}
 			if (i == 1) {
-				int nterm;
-				cin >> nterm;
-				v2.push_back(nterm);
-				while (nterm--) {
+				p2.n = nterm;
+				for (int i = 0; i < nterm*2; i++) {
+					p2.co[i] = 0;
+					p2.in[i] = -1;
+				}
+				for (int j = 0; j < nterm; j++) {
 					int a, b;
 					cin >> a >> b;
-					v2.push_back(a);
-					v2.push_back(b);
+					p2.co[j] = a;
+					p2.in[j] = b;
 				}
 
-				p2 = make_poly(v2);
-				/*print_poly(p2);
+				/*cout << p2.n << endl;
+				for (int j = 0; j < nterm; j++) {
+					cout << p2.co[j] << " ";
+				}
+				cout << endl;
+				for (int j = 0; j < nterm; j++) {
+					cout << p2.in[j] << " ";
+				}
 				cout << endl;*/
 			}
+			
 		}
-		poly p3 = poly_minus(p1, p2);
-		print_poly(p3);
+		/*cout << endl;
+		cout << endl;
+		cout << endl;*/
+		c = poly_sub(p1, p2);
+		/*cout << c.n << endl;
+		for (int j = 0; j < c.n; j++) {
+			cout << c.co[j] << " ";
+		}
+		cout << endl;
+		for (int j = 0; j < c.n; j++) {
+			cout << c.in[j] << " ";
+		}
+		cout << endl;
+		cout << endl;
+		cout << endl;*/
+
+		print_poly(c);
+		cout << "\n";
 	}
 }
